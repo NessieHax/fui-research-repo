0e2dbc7ff5f150f4e92610f0e7e75af3c12e1024
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-19 20:38:18 +0100

    Fixed typo

--- python/fuiInfoDumper.py
+++ python/fuiInfoDumper.py
@@ -10,7 +10,7 @@ def usage() -> None:
     print(f"Options:\n\t -d, --dump [folder](Optional)\t\tDumps images(with symbol name) into a folder(default is 'output').")
     print(f"\n\t -rd, --raw-dump [folder](Optional)\tDumps all images into a folder(default is 'output').")
     print(f"\n\t -f, --find name\tPrints all images that contain [name].")
-    print(f"\n\t -h, --help name\tDisplay this message.")
+    print(f"\n\t -h, --help\tDisplay this message.")
 
 
 def main():
@@ -33,39 +33,7 @@ def main():
             elif sys.argv[2] == "--find" or sys.argv[2] == "-f" and argc > 3:
                 parser.get_image_by_name(sys.argv[3])
 
-        # print(f"Imported Assets: {parser.get_imported_assets()}\n")
-        # print(f"Symbol Names: {parser.get_symbols()}\n")
-        print(f"Reference Names: {parser.get_references()}\n")
 
-        # print([x for x in parser.get_symbols() if x.obj_type != 3])
-        # print([x for x in parser.get_timelines() if x.unkn_0x0 != -1])
-        # print([parser.get_timelines()[x.index] for x in parser.get_symbols() if x.obj_type == 2])
-
-        # print(f"Bitmaps: {parser.get_bitmaps()}\n")
-        print(f"Shapes: {parser.get_shapes()}\n")
-        print("Shape count:", len(parser.get_shapes()))
-        print(f"Shape Components: {parser.get_shape_components()}\n")
-        print("Shape Component count:", len(parser.get_shape_components()))
-
-        # print(f"Timeline : {parser.get_timelines()}\n")
-        # print(f"Timeline Event({len(parser.get_timeline_events())}): {parser.get_timeline_events()}\n")
-        # print(f"Timeline Event Names: {parser.get_timeline_event_names()}\n")
-
-        for data in parser.get_timelines():
-            print(f"'{parser.get_timeline_frames()[data.frame_name_index].frame_name}' -> {data}")
-        print()
-        for data in parser.get_timeline_events():
-            if data.name_index > -1: print(parser.get_timeline_event_names()[data.name_index].name, ":", data)
-        # print(len(parser.get_edittext()))
-        # for data in parser.get_edittext():
-        #     print(data)
-
-        # print(f"Timeline Frame Names: {parser.get_timeline_frames()}\n")
-        # print(f"Timeline Actions: {parser.get_timeline_actions()}\n")
-        # print(f"Font Names: {parser.get_fonts()}\n")
-        # print(f"Vert : {parser.get_vert()}\n")
-        # print(f"Vert : {len(parser.get_vert())}\n")
-        # print(parser.validate_content_size())
 
 
 if __name__ == "__main__" and argc > 1:
