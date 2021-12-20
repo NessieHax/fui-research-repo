3064d22ad40718b2bf99e0f8495c274b07fc7d7b
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-20 23:38:41 +0100

    Updated variable name in fuiReference

--- python/fuiDataStructures/fuiReference.py
+++ python/fuiDataStructures/fuiReference.py
@@ -7,11 +7,11 @@ class fuiReference:
     fmt = "<i64si"
 
     symbol_index:int = field(default_factory=int)
-    refernce_name:str = field(default_factory=str)
+    reference:str = field(default_factory=str)
     fui_file_index:int = field(default_factory=int)#
 
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
         self.symbol_index = data[0]
-        self.refernce_name = data[1].decode('UTF-8').strip("\0")
+        self.reference = data[1].decode('UTF-8').strip("\0")
         self.fui_file_index = data[2]
