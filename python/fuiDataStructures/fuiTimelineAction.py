1b7b545ef1f74a3c70090dffe187e2c8026c791c
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-27 21:43:42 +0100

    Update fuiTimelineAction, Add eAction

--- python/fuiDataStructures/fuiTimelineAction.py
+++ python/fuiDataStructures/fuiTimelineAction.py
@@ -5,14 +5,19 @@ import struct
 class fuiTimelineAction:
     fmt = "<2h64s64s"
 
-    unkn_0x0:int = field(default_factory=int)
+    class eAction:
+        prev_event = 0 #! links action_arg0 to action_arg1 as the next event(focus)
+        next_event = 1 #! links action_arg0 to action_arg1 as the next event(focus)
+        cycle = 13
+
+    action_type:int = field(default_factory=int)
     unkn_0x2:int = field(default_factory=int)
-    action_name:str = field(default_factory=str)
-    unk_str:str = field(default_factory=str)
+    action_arg0:str = field(default_factory=str)
+    action_arg1:str = field(default_factory=str)
 
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
-        self.unkn_0x0 = data[0]
+        self.action_type = data[0]
         self.unkn_0x2 = data[1]
-        self.action_name = data[2].decode('UTF-8').strip("\0")
-        self.unk_str = data[3].decode('UTF-8').strip("\0")
+        self.action_arg0 = data[2].decode('UTF-8').strip("\0")
+        self.action_arg1 = data[3].decode('UTF-8').strip("\0")
