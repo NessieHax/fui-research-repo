7dbb6420e93453c9278427404f59e6f26aabe4fc
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-27 21:46:00 +0100

    Update fuiDumper - added more commands

--- python/fuiInfoDumper.py
+++ python/fuiInfoDumper.py
@@ -4,11 +4,16 @@ from fuiParser import fuiParser
 
 def main():
     argvparser = argparse.ArgumentParser()
-    argvparser.add_argument("fuiFile", type=str, help="FUI File to parse.")
+    argvparser.add_argument("fuiFile", type=str, help="FUI File to be Parsed.")
+    argvparser.add_argument("-sym", "--symbols", action="store_true", default=False, help="Prints out all Symbols contained in a file")
     argvparser.add_argument("-d", "--dump", action="store_true", default=False, help="Dumps images(with symbol name) into a folder(default is 'output')")
+    #! TODO: impl this
+    # argvparser.add_argument("-r", "--replace", metavar=("Bitmap_symbol","image_file"), nargs=2, choices=["img_format"], action="store", default=False, help="Replaces given name with the passed image")
     argvparser.add_argument("-rd","--raw-dump", action="store_true", default=False, help="Dumps all images into a folder(default is 'output')")
-    argvparser.add_argument("-f","--find", metavar="name", type=str, action="store", help="Prints out fuiBitmap's that contain [name]")
+    argvparser.add_argument("-f","--find", metavar="name", type=str, action="store", help="Prints out fui Element's that contain [name]")
+    argvparser.add_argument("-frefs","--find-references", dest="find_ref", metavar="folder", type=str, action="store", help="Tries to find references contained in Import files")
     parsedagrs = argvparser.parse_args()
+    if not parsedagrs.fuiFile.endswith(".fui"): raise Exception("Not a fui file")
     with open(parsedagrs.fuiFile,"rb") as fui:
         parser = fuiParser(fui.read())
         parser.parse() #! needs to be called before you're able to access data via the getter
@@ -19,26 +24,43 @@ def main():
         if parsedagrs.raw_dump:
             parser.dump_raw(f"{os.getcwd()}/{output_path}")
         if parsedagrs.find:
-            parser.get_image_by_name(parsedagrs.find)
+            parser.find(parsedagrs.find)
+        if parsedagrs.symbols:
+            print("Symbols:")
+            for symbol in parser.get_symbols():
+                print(symbol.name)
 
-        print(f"Imported Assets: {parser.get_imported_assets()}\n")
+        if parsedagrs.find_ref:
+            if not os.path.exists(parsedagrs.find_ref) or len(parser.get_imported_assets()) == 0: return
+            fui_parser = []
+            for file in os.listdir(parsedagrs.find_ref):
+                if file.endswith(".fui"):
+                    abs_path = os.path.join(os.path.abspath(parsedagrs.find_ref),file)
+                    with open(abs_path, "rb") as fui:
+                        fui_parser.append((file,fuiParser(fui.read())))
+
+            for import_file in parser.get_imported_assets():
+                for (file_name, cur_parser) in fui_parser:
+                    if cur_parser.header.swf_name == import_file.import_name:
+                        print(f"Found import name({import_file.import_name}) in {file_name}")
+
+        # print(f"Imported Assets: {parser.get_imported_assets()}\n")
         # print(f"Symbol Names: {parser.get_symbols()}\n")
-        print(f"Reference Names: {parser.get_references()}\n")
-
-        # print([(i, x) for i,x in enumerate(parser.get_symbols()) if x.obj_type != 3])
+        # print(f"Reference Names: {parser.get_references()}\n")
+        # print(f"Reference Names: {len(parser.get_references())}\n")
 
         # print(f"Shapes: {parser.get_shapes()}\n")
         # print(f"Shape Components: {parser.get_shape_components()}\n")
 
-        print("Shape count:", len(parser.get_shapes()))
-        print("Shape Component count:", len(parser.get_shape_components()))
+        # print(f"Shape count: {len(parser.get_shapes())}\n")
+        # print("Shape Component count:", len(parser.get_shape_components()))
 
-        parser.print_timeline_tree()
+        # print(len(parser.get_timeline_actions()))
+        # print(parser.get_start_offset_of("fuiTimelineAction"))
+
+        # parser.print_timeline_tree()
         
-        # print(parser.get_edittext())
-        # print(len(parser.get_timeline_events()))
-        # for data in parser.get_timeline_events():
-        #     print(data)
+        # print(len(parser.get_edittext()))
 
         # print(f"Font Names: {parser.get_fonts()}\n")
         # print(f"Vert : {parser.get_vert()}\n")
