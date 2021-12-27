8e73364ba71a511b602de51dcd109c366836cfde
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-27 21:41:05 +0100

    Update fuiBitmap members and Add eBitmapFormat

--- python/fuiDataStructures/fuiBitmap.py
+++ python/fuiDataStructures/fuiBitmap.py
@@ -5,8 +5,15 @@ import struct
 class fuiBitmap:
     fmt = "<8i"
 
+    class eBitmapFormat:
+        PNG_WITH_ALPHA_DATA:int = 1 #! fully ignored
+        PNG_NO_ALPHA_DATA:int = 3   #! fully ignored
+        JPEG_NO_ALPHA_DATA:int = 6
+        JPEG_UNKNOWN:int = 7 #! TODO: find name
+        JPEG_WITH_ALPHA_DATA:int = 8
+
     unkn_0x0:int = field(default_factory=int)
-    flags:int = field(default_factory=int)
+    format:int = field(default_factory=int)
     width:int = field(default_factory=int)
     height:int = field(default_factory=int)
     offset:int = field(default_factory=int)
@@ -17,10 +24,13 @@ class fuiBitmap:
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
         self.unkn_0x0 = data[0]
-        self.flags = data[1] #! bitmask (1 << 3) == insert zlib alpha channel data
+        self.format = data[1]
         self.width = data[2]
         self.height = data[3]
         self.offset = data[4]
         self.size = data[5]
-        self.zlib_data_offset = data[6] #! zlib compressed size ??
+        self.zlib_data_offset = data[6]
         self.unkn_0x1c = data[7] #! set to -1 if they was an error at runtime
+
+    def pack(self) -> bytearray:
+        return struct.pack(self.fmt, self.unkn_0x0, self.format, self.width, self.height, self.offset, self.size, self.zlib_data_offset, self.unkn_0x1c)
