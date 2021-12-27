b475ac01e1682896cb3b9f0b4d7fc485a8cb1032
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-27 21:41:50 +0100

    Update fuiTimelineEvent

--- python/fuiDataStructures/fuiTimelineEvent.py
+++ python/fuiDataStructures/fuiTimelineEvent.py
@@ -8,22 +8,22 @@ from fuiDataStructures.fuiColorTransform import fuiColorTransform
 class fuiTimelineEvent:
     fmt = "<6h6f8fI"
 
-    unkn_0x0:int = field(default_factory=int)
-    unkn_0x2:int = field(default_factory=int)
+    event_type:int = field(default_factory=int)
+    obj_type:int = field(default_factory=int)
     unkn_0x4:int = field(default_factory=int)
-    unkn_0x6:int = field(default_factory=int)
+    index:int = field(default_factory=int)
     unkn_0x8:int = field(default_factory=int)
     name_index:int = field(default_factory=int)
-    matrix:fuiMatrix = field(default_factory=fuiMatrix)
-    ColorTransform:fuiColorTransform = field(default_factory=fuiColorTransform,repr=False)
-    color:int = field(default_factory=int,repr=False)
+    matrix:fuiMatrix = field(default_factory=fuiMatrix, repr=False)
+    ColorTransform:fuiColorTransform = field(default_factory=fuiColorTransform, repr=False)
+    color:int = field(default_factory=int, repr=False)
 
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
-        self.unkn_0x0 = data[0]
-        self.unkn_0x2 = data[1]
+        self.event_type = data[0]
+        self.obj_type = data[1]
         self.unkn_0x4 = data[2]
-        self.unkn_0x6 = data[3]
+        self.index = data[3]
         self.unkn_0x8 = data[4]
         self.name_index = data[5]
         self.matrix = fuiMatrix(data[6], data[7], data[8], data[9], data[10], data[11])
