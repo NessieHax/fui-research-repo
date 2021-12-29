ae76c2740cf0f22887df9df797c68025c7276866
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-29 19:37:54 +0100

    Update fuiFontName

--- python/fuiDataStructures/fuiFontName.py
+++ python/fuiDataStructures/fuiFontName.py
@@ -5,7 +5,7 @@ import struct
 class fuiFontName:
     fmt = "<i64si64s2i64s2i44s"
 
-    unkn_0x0:int = field(default_factory=int)
+    id:int = field(default_factory=int)
     font_name:str = field(default_factory=str)
     unkn_0x44:int = field(default_factory=int)
     unkn_str0:str = field(default_factory=str)
@@ -18,13 +18,13 @@ class fuiFontName:
 
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
-        self.unkn_0x0 = data[0]
+        self.id = data[0]
         self.font_name = data[1].decode('UTF-8').strip("\0")
-        self.unkn_0x44 = data[2]
-        self.unkn_str0 = data[3].decode('UTF-8').strip("\0")
-        self.unkn_0x88 = data[4]
-        self.unkn_0x8c = data[5]
-        self.unkn_str1 = data[6].decode('UTF-8').strip("\0")
-        self.unkn_0xd0 = data[7]
-        self.unkn_0xd4 = data[8]
-        self.unkn_str2 = data[9].decode('UTF-8').strip("\0")
+        self.unkn_0x44 = data[2] #! unused ??
+        self.unkn_str0 = data[3].replace(b"\0",b"") #! unused ??
+        self.unkn_0x88 = data[4] #! unused ??
+        self.unkn_0x8c = data[5] #! unused ??
+        self.unkn_str1 = data[6].replace(b"\0",b"") #! unused ??
+        self.unkn_0xd0 = data[7] #! unused ??
+        self.unkn_0xd4 = data[8] #! unused ??
+        self.unkn_str2 = data[9].replace(b"\0",b"") #! unused ??
