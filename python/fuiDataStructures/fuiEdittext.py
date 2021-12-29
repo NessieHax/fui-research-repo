711c19096e7fc9c06f0385e551f5c7c9469784b7
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-12-29 19:38:21 +0100

    Update fuiEdittext

--- python/fuiDataStructures/fuiEdittext.py
+++ python/fuiDataStructures/fuiEdittext.py
@@ -1,30 +1,33 @@
 from dataclasses import dataclass, field
-import struct
+import struct, re
 
 from fuiDataStructures.fuiRect import fuiRect
 
 @dataclass(init=False)
 class fuiEdittext:
-    fmt = "<i4fifI6i256s"
+    fmt = "<i4fifIb3x3if4?256s"
 
-    unkn_0x0:int = field(default_factory=int)
+    unkn_0x0:int = field(default_factory=int) #! unused!
     rect:fuiRect = field(default_factory=fuiRect)
-    unkn_0x14:int = field(default_factory=int)
+    font_id:int = field(default_factory=int)
     unkn_0x18:float = field(default_factory=float)
     color:int = field(default_factory=int)
-    unkn_0x20:int = field(default_factory=int)
+    unkn_0x20:int = field(default_factory=int) #! alignment stuff 1 byte
     unkn_0x24:int = field(default_factory=int)
     unkn_0x28:int = field(default_factory=int)
     unkn_0x2c:int = field(default_factory=int)
-    unkn_0x30:int = field(default_factory=int)
-    unkn_0x34:int = field(default_factory=int)
+    unkn_0x30:float = field(default_factory=float) #! unused ?
+    unkn_0x34:bool = field(default_factory=bool) #! bool flag
+    unkn_0x35:bool = field(default_factory=bool) #! bool flag
+    unkn_0x36:bool = field(default_factory=bool) #! bool flag unused!
+    unkn_0x37:bool = field(default_factory=bool) #! bool flag unused!
     html_str:str = field(default_factory=str)
 
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
         self.unkn_0x0 = data[0]
         self.rect = fuiRect(data[1], data[2], data[3], data[4])
-        self.unkn_0x14 = data[5]
+        self.font_id = data[5]
         self.unkn_0x18 = data[6]
         self.color = data[7]
         self.unkn_0x20 = data[8]
@@ -33,4 +36,7 @@ class fuiEdittext:
         self.unkn_0x2c = data[11]
         self.unkn_0x30 = data[12]
         self.unkn_0x34 = data[13]
-        self.html_str = data[14]
+        self.unkn_0x35 = data[14]
+        self.unkn_0x36 = data[15]
+        self.unkn_0x37 = data[16]
+        self.html_str = data[17].decode('UTF-8', "ignore")
