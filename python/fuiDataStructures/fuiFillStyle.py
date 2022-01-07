688940b56277acba921b3b18e94d85dfd88aa2de
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-01-07 18:21:30 +0100

    Update fuiFillStyle

--- python/fuiDataStructures/fuiFillStyle.py
+++ python/fuiDataStructures/fuiFillStyle.py
@@ -8,14 +8,14 @@ class fuiFillStyle:
 
     unkn_0x0:int = field(default_factory=int)
     color:int = field(default_factory=int)
-    unkn_0x8:int = field(default_factory=int)
+    bitmap_index:int = field(default_factory=int)
     matrix:fuiMatrix = field(default_factory=fuiMatrix)
 
-    def __init__(self, unkn_0x0:int, color:int, unkn_0x8:int, matrix:fuiMatrix):
-        self.unkn_0x0 = unkn_0x0
+    def __init__(self, unkn_0x0:int, color:int, bitmap_index:int, matrix:fuiMatrix):
+        self.unkn_0x0 = unkn_0x0 #! 1 == fillWithColor | 5 == use bitmap | 3 == scale bitmap by matrix ??
         self.color = color
-        self.unkn_0x8 = unkn_0x8
+        self.bitmap_index = bitmap_index
         self.matrix = matrix
 
     def __iter__(self):
-        return iter([self.unkn_0x0, self.color, self.unkn_0x8, *self.matrix])
+        return iter([self.unkn_0x0, self.color, self.bitmap_index, *self.matrix])
