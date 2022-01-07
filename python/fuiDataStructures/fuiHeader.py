d24bda1ccc45c9e944160310fd77a81e854b13e5
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-01-07 10:25:36 +0100

    Move fuiHeader.py

--- python/fuiDataStructures/fuiHeader.py
+++ python/fuiDataStructures/fuiHeader.py
@@ -3,25 +3,26 @@ import struct
 from fuiDataStructures.fuiRect import fuiRect
 
 class fuiHeader:
-    fmt = "<4s4xI64s15i4f"
+    fmt = "<8sI64s15i4f"
     header_size:int = struct.calcsize(fmt)
     def __init__(self, raw_data:bytes):
         data = struct.unpack(self.fmt, raw_data)
-        self.version = data[0][0]
-        self.identifier:str = data[0][1:4] #! loaded : "FUI" | stored : "IUF"
-        self.content_size:int = data[1] #! header_size + content_size = whole file size
-        self.swf_name:str = data[2].decode("UTF-8").strip("\0")
+        self.signature:bytes = data[0]
+        self.content_size:int = data[1]
+        self.import_name:str = data[2].decode("UTF-8").strip("\0")
         self.data_counts:list = [count for count in data[3:18]]
         self.rect:fuiRect = fuiRect(data[18], data[19], data[20], data[21])
 
+    def pack(self) -> bytearray:
+        return bytearray(struct.pack(self.fmt, self.signature, self.content_size, self.import_name.encode("UTF-8"), *self.data_counts, *self.rect))
+
     def __repr__(self):
         return f"""
-Version: {self.version}
-Identifier: {self.identifier}
+Version: {self.signature[0]}
+Signature: {self.signature}
 Content Size: {self.content_size}
 Header Size: {self.header_size}
-Swf Name: {self.swf_name}
-Stage: {self.rect}
+Import Name: {self.import_name}
 Stage Size: {self.rect.get_size()}
+{self.data_counts}
 """
-# Object Counts: {self.data_counts}
