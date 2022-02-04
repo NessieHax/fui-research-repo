a1fd07ba4b9abe7f9ed5ce6849819b93ed5c0a69
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-02-04 15:52:33 +0100

    Update fuiShape variable name

--- python/fuiDataStructures/fuiShape.py
+++ python/fuiDataStructures/fuiShape.py
@@ -11,14 +11,14 @@ class fuiShape(fuiObject):
     unk_0x0:int = field(default_factory=int)
     component_index:int = field(default_factory=int)
     component_count:int = field(default_factory=int)
-    scale:fuiRect = field(default_factory=fuiRect)
+    area:fuiRect = field(default_factory=fuiRect)
 
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
         self.unk_0x0 = data[0]
         self.component_index = data[1]
         self.component_count = data[2]
-        self.scale = fuiRect(data[3], data[4], data[5], data[6])
+        self.area = fuiRect(data[3], data[4], data[5], data[6])
 
     def pack(self) -> bytearray:
-        return bytearray(struct.pack(self.fmt, self.unk_0x0, self.component_index, self.component_count, *self.scale))
+        return bytearray(struct.pack(self.fmt, self.unk_0x0, self.component_index, self.component_count, *self.area))
