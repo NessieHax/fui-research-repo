81c7c8cb5ec06e6600fa93c441d54446e6c895cb
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-04-04 00:04:54 +0200

    Update example

--- python/example.py
+++ python/example.py
@@ -3,10 +3,8 @@ import sys
 from fuiParser import fuiParser
 
 def main():
-    with open(sys.argv[1],"rb") as fui:
-        parser = fuiParser(fui.read())
-        print(parser.header)
-        print(parser)
+    parser = fuiParser(sys.argv[1])
+    print(parser)
 
 
 if __name__ == "__main__" and len(sys.argv) > 1:
