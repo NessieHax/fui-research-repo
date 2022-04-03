f06320859bfa67fc504785a86348da87c952e262
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-04-03 23:59:49 +0200

    Fix fuiTimelineAction format

--- python/fuiDataStructures/fuiTimelineAction.py
+++ python/fuiDataStructures/fuiTimelineAction.py
@@ -5,14 +5,10 @@ from fuiDataStructures.fuiObject import fuiObject
 
 @dataclass(init=False)
 class fuiTimelineAction(fuiObject):
-    fmt = "<2h64s64s"
-
-    class eAction:
-        prev_event = 0 #! links action_arg0 to action_arg1 as the next event(focus)
-        next_event = 1 #! links action_arg0 to action_arg1 as the next event(focus)
-        cycle = 13
+    fmt = "<2bh64s64s"
 
     action_type:int = field(default_factory=int)
+    unkn_0x1:int = field(default_factory=int)
     unkn_0x2:int = field(default_factory=int)
     action_arg0:str = field(default_factory=str)
     action_arg1:str = field(default_factory=str)
@@ -20,9 +16,10 @@ class fuiTimelineAction(fuiObject):
     def __init__(self, raw_bytes:bytes):
         data = struct.unpack(self.fmt, raw_bytes)
         self.action_type = data[0]
-        self.unkn_0x2 = data[1]
-        self.action_arg0 = data[2].decode('UTF-8').strip("\0")
-        self.action_arg1 = data[3].decode('UTF-8').strip("\0")
+        self.unkn_0x1 = data[1]
+        self.unkn_0x2 = data[2]
+        self.action_arg0 = data[3].decode('UTF-8').strip("\0")
+        self.action_arg1 = data[4].decode('UTF-8').strip("\0")
 
     def pack(self) -> bytearray:
-        return bytearray(struct.pack(self.fmt, self.action_type, self.unkn_0x2, self.action_arg0.encode("UTF-8"), self.action_arg1.encode("UTF-8")))
+        return bytearray(struct.pack(self.fmt, self.action_type, self.unkn_0x1, self.unkn_0x2, self.action_arg0.encode("UTF-8"), self.action_arg1.encode("UTF-8")))
